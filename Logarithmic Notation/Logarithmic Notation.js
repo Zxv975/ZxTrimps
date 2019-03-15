c9c1941802858c86ac23383205680d7ef04b5893
Zxv975 <zxbryte@gmail.com>
2019-03-15 11:35:32 +1100

    Added Logarithmic Notation

---
+++ Logarithmic Notation/Logarithmic Notation.js
@@ -0,0 +1,71 @@
+function prettify(number) {
+	var numberTmp = number;
+	if (!isFinite(number)) return "<span class='icomoon icon-infinity'></span>";
+	if (number >= 1000 && number < 10000) return Math.floor(number);
+	if (number == 0) return prettifySub(0);
+	if (number < 0) return "-" + prettify(-number);
+	if (number < 0.005) return (+number).toExponential(2);
+
+	var base = Math.floor(Math.log(number)/Math.log(1000));
+	if (base <= 0) return prettifySub(number);
+    
+    // This part is used for logarithmic notation only
+	var mantissa = 10;
+	var exponent = Math.log(number) / Math.log(logarithmicBase);
+    
+	number /= Math.pow(1000, base);
+	if (number >= 999.5) {
+		// 999.5 rounds to 1000 and we don’t want to show “1000K” or such
+		number /= 1000;
+		++base;
+	}
+	if (game.options.menu.standardNotation.enabled == 3){
+		var suffices = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+		if (base <= suffices.length) suffix = suffices[base -1];
+		else {
+			var suf2 = (base % suffices.length) - 1;
+			if (suf2 < 0) suf2 = suffices.length - 1;
+			suffix = suffices[Math.ceil(base / suffices.length) - 2] + suffices[suf2];
+		}
+	}
+	else if(game.options.menu.standardNotation.enabled == 5) {
+		return mantissa + "^" + prettifySub(exponent);
+	}
+	else {
+		var suffices = [
+			'K', 'M', 'B', 'T', 'Qa', 'Qi', 'Sx', 'Sp', 'Oc', 'No', 'Dc', 'Ud',
+            'Dd', 'Td', 'Qad', 'Qid', 'Sxd', 'Spd', 'Od', 'Nd', 'V', 'Uv', 'Dv',
+            'Tv', 'Qav', 'Qiv', 'Sxv', 'Spv', 'Ov', 'Nv', 'Tg', 'Utg', 'Dtg', 'Ttg',
+            'Qatg', 'Qitg', 'Sxtg', 'Sptg', 'Otg', 'Ntg', 'Qaa', 'Uqa', 'Dqa', 'Tqa',
+            'Qaqa', 'Qiqa', 'Sxqa', 'Spqa', 'Oqa', 'Nqa', 'Qia', 'Uqi', 'Dqi',
+            'Tqi', 'Qaqi', 'Qiqi', 'Sxqi', 'Spqi', 'Oqi', 'Nqi', 'Sxa', 'Usx',
+            'Dsx', 'Tsx', 'Qasx', 'Qisx', 'Sxsx', 'Spsx', 'Osx', 'Nsx', 'Spa',
+            'Usp', 'Dsp', 'Tsp', 'Qasp', 'Qisp', 'Sxsp', 'Spsp', 'Osp', 'Nsp',
+            'Og', 'Uog', 'Dog', 'Tog', 'Qaog', 'Qiog', 'Sxog', 'Spog', 'Oog',
+            'Nog', 'Na', 'Un', 'Dn', 'Tn', 'Qan', 'Qin', 'Sxn', 'Spn', 'On',
+            'Nn', 'Ct', 'Uc'
+		];
+		var suffix;
+		if (game.options.menu.standardNotation.enabled == 2 || (game.options.menu.standardNotation.enabled == 1 && base > suffices.length) || (game.options.menu.standardNotation.enabled == 4 && base > 31))
+			suffix = "e" + ((base) * 3);
+		else if (game.options.menu.standardNotation.enabled && base <= suffices.length)
+			suffix = suffices[base-1];
+		else
+		{
+			var exponent = parseFloat(numberTmp).toExponential(2);
+			exponent = exponent.replace('+', '');
+			return exponent;
+		}
+	}
+	return prettifySub(number) + suffix;
+}
+
+function prettifySub(number){
+	number = parseFloat(number);
+	var floor = Math.floor(number);
+	if (number === floor) // number is an integer, just show it as-is
+		return number;
+	var precision = 3 - floor.toString().length; // use the right number of digits
+
+	return number.toFixed(3 - floor.toString().length);
+}
