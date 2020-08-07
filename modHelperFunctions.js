4321a3695fb991ea2f1fecd6250bb9cb3901a9ce
Zxv975 <zxbryte@gmail.com>
2020-08-07 19:09:27 +1000

    Added more description to helper functions, and reworked equality slider

--- modHelperFunctions.js
+++ modHelperFunctions.js
@@ -1,4 +1,5 @@
 function findPositionInFunction(f, searchTerms, offset = 1, stringPos = 0) { // Search a function f for the first instance of searchTerms[0], then find (relative to the first index) the position of searchTerms[1], and so on until searchTerms[n-1]. searchTerms[0] should be the most specific term (to get the search as close as possible), and searchTerms[1] onwards should get successively more broad, but be ordered such that all unwanted matchings for searchTerms[i] will be passed over by searchTerms[0] through searchTerms[i-1]. For example, searchTerms[0] could be an if statement conditional, and searchTerms[1] would be the next closing brace. The result would then be the position of the first closing brace after a specific if statement.
+// Note that offset is relative to the **start** of the last matched string in searchTerms[]
 	if(searchTerms.length == 0) 
 		return stringPos + offset;
 	var tempTerm = searchTerms[0];
@@ -7,5 +8,6 @@ function findPositionInFunction(f, searchTerms, offset = 1, stringPos = 0) { // 
 }
 
 function insertCode(f, position, codeString, offset = 0) {
+// Note that offset is relative to the position specified by "position".
 	return Function(`return ${f.toString().slice(0, position)}${codeString}${f.toString().slice(position + offset)}`)()
 }
